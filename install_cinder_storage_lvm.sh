b5be820e2fe6ef467f6c82ba689f5fe536bb81f3
Mehmet Tecer <mehmettecer@gmail.com>
2016-08-23 13:51:04 -0400

    Updating cinder volume service names

--- install_cinder_storage_lvm.sh
+++ install_cinder_storage_lvm.sh
@@ -32,16 +32,18 @@ function configure_cinder_storage()
 	__start_service lvm2-lvmetad
 	__start_service target
 
-	openstack-config --set ${cinder_config_file} DEFAULT storage_availability_zone "${__storage_name}"
+	# openstack-config --set ${cinder_config_file} DEFAULT storage_availability_zone "${__storage_name}"
+	# openstack-config --set ${cinder_config_file} DEFAULT enabled_backends "${__storage_name}"
 
-	openstack-config --set ${cinder_config_file} DEFAULT enabled_backends "${__storage_name}"
+	openstack-config --set ${cinder_config_file} DEFAULT storage_availability_zone nova
+	openstack-config --set ${cinder_config_file} DEFAULT enabled_backends LVM_iSCSI
 
-	openstack-config --set ${cinder_config_file} ${__storage_name} iscsi_helper lioadm
-	openstack-config --set ${cinder_config_file} ${__storage_name} iscsi_protocol iscsi
-	openstack-config --set ${cinder_config_file} ${__storage_name} volume_driver cinder.volume.drivers.lvm.LVMVolumeDriver
-	openstack-config --set ${cinder_config_file} ${__storage_name} iscsi_ip_address ${my_ip}
-	openstack-config --set ${cinder_config_file} ${__storage_name} volume_group cinder-volumes
-	openstack-config --set ${cinder_config_file} ${__storage_name} volume_backend_name ${__storage_name}
+	openstack-config --set ${cinder_config_file} LVM_iSCSI iscsi_helper lioadm
+	openstack-config --set ${cinder_config_file} LVM_iSCSI iscsi_protocol iscsi
+	openstack-config --set ${cinder_config_file} LVM_iSCSI volume_driver cinder.volume.drivers.lvm.LVMVolumeDriver
+	openstack-config --set ${cinder_config_file} LVM_iSCSI iscsi_ip_address ${my_ip}
+	openstack-config --set ${cinder_config_file} LVM_iSCSI volume_group cinder-volumes
+	openstack-config --set ${cinder_config_file} LVM_iSCSI volume_backend_name LVM_iSCSI
 
 	( cinder type-create ${__storage_name}
 	cinder type-key ${__storage_name} set volume_backend_name=${__storage_name} ) > /dev/null 2>&1
