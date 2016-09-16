c9b510b88a95f9df158a9e2001f91984e67f64a2
Mehmet Tecer <mehmettecer@gmail.com>
2016-09-16 15:08:00 -0400

    Enabling L3 agent on controller nodes

--- install_l2_networking.sh
+++ install_l2_networking.sh
@@ -1,29 +1,55 @@
 function deploy_l2_vlan_infrastructure_controller()
 {
+    __ml2_config_file='/etc/neutron/plugins/ml2/ml2_conf.ini'
+    __ovs_config_file='/etc/neutron/plugins/ml2/openvswitch_agent.ini'
+
     openstack-config --set /etc/neutron/neutron.conf DEFAULT core_plugin ml2
     openstack-config --set /etc/neutron/neutron.conf DEFAULT service_plugins router
 
-    openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 type_drivers flat,vlan
-    openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 tenant_network_types
-    openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 mechanism_drivers openvswitch
-    openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 extension_drivers port_security
+    # ML2
+    #  [ml2]
+    openstack-config --set ${__ml2_config_file} ml2 type_drivers 'vlan,vxlan'
+    openstack-config --set ${__ml2_config_file} ml2 tenant_network_types 'vxlan'
+    openstack-config --set ${__ml2_config_file} ml2 mechanism_drivers 'openvswitch,l2population'
+    openstack-config --set ${__ml2_config_file} ml2 extension_drivers 'port_security'
+    #  [ml2_type_vlan]
+    openstack-config --set ${__ml2_config_file} ml2_type_vlan network_vlan_ranges 'provider'
+    #  [ml2_type_vxlan]
+    openstack-config --set ${__ml2_config_file} ml2_type_vxlan vni_ranges '4096:16777215'
+    openstack-config --set ${__ml2_config_file} ml2_type_vxlan vxlan_group '224.0.0.1'
+    #  [securitygroup]
+    openstack-config --set ${__ml2_config_file} securitygroup enable_ipset 'True'
+    openstack-config --set ${__ml2_config_file} securitygroup firewall_driver iptables_hybrid
+    openstack-config --set ${__ml2_config_file} securitygroup enable_security_group 'True'
 
-    openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_flat flat_networks provider
+    # OpenVSWitch
+    #  [agent]
+    openstack-config --set ${__ovs_config_file} agent tunnel_types 'vxlan'
+    openstack-config --set ${__ovs_config_file} agent l2_population 'True'
+    #  [ovs]
+    openstack-config --set ${__ovs_config_file} ovs local_ip "$(hostname --ip-address)"
+    openstack-config --set ${__ovs_config_file} ovs bridge_mappings 'provider:br-provider'
+    #  [securitygroup]
+    openstack-config --set ${__ovs_config_file} securitygroup firewall_driver 'iptables_hybrid'
+    openstack-config --set ${__ovs_config_file} securitygroup enable_security_group 'True'
 
-    openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_vlan network_vlan_ranges provider
-
-    openstack-config --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup firewall_driver iptables_hybrid
-
-    openstack-config --set /etc/neutron/plugins/ml2/openvswitch_agent.ini ovs bridge_mappings provider:br-provider
-
-    openstack-config --set /etc/neutron/plugins/ml2/openvswitch_agent.ini securitygroup firewall_driver iptables_hybrid
-
+    # DHCP
     openstack-config --set /etc/neutron/dhcp_agent.ini DEFAULT interface_driver neutron.agent.linux.interface.OVSInterfaceDriver
     openstack-config --set /etc/neutron/dhcp_agent.ini DEFAULT enable_isolated_metadata True
     openstack-config --set /etc/neutron/dhcp_agent.ini DEFAULT dhcp_lease_duration 120
 
+    # Metadata
     openstack-config --set /etc/neutron/metadata_agent.ini DEFAULT nova_metadata_ip ${api_address}
     openstack-config --set /etc/neutron/metadata_agent.ini DEFAULT metadata_proxy_shared_secret ${metadata_proxy_shared_secret}
+
+    # L3
+    openstack-config --set ${neutron_config_file} DEFAULT l3_ha 'True'
+    openstack-config --set ${neutron_config_file} DEFAULT allow_automatic_l3agent_failover 'True'
+    openstack-config --set ${neutron_config_file} DEFAULT min_l3_agents_per_router '2'
+    openstack-config --set ${neutron_config_file} DEFAULT max_l3_agents_per_router '2'
+
+    openstack-config --set /etc/neutron/l3_agent.ini DEFAULT interface_driver 'neutron.agent.linux.interface.OVSInterfaceDriver'
+    openstack-config --set /etc/neutron/l3_agent.ini DEFAULT external_network_bridge
 
     ( su -s /bin/sh -c "neutron-db-manage --config-file /etc/neutron/neutron.conf \
       --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head" neutron ) > /dev/null 2>&1
@@ -41,7 +67,7 @@ function deploy_l2_vlan_infrastructure_controller()
         echo 'TYPE="OVSBridge"'
         echo 'BOOTPROTO="none"'
         echo 'HOTPLUG="no"'
-     ) > /etc/sysconfig/network-scripts/ifcfg-br-provider
+    ) > /etc/sysconfig/network-scripts/ifcfg-br-provider
     _md5sum_updated=$(md5sum /etc/sysconfig/network-scripts/ifcfg-br-provider)
 
     if [[ ${_md5sum_original} != ${_md5sum_updated} ]]; then
@@ -55,17 +81,29 @@ function deploy_l2_vlan_infrastructure_controller()
     __enable_service neutron-netns-cleanup
     __enable_service neutron-ovs-cleanup
     __enable_service neutron-dhcp-agent
+    __enable_service neutron-l3-agent
 
     __start_service neutron-metadata-agent
     __start_service neutron-dhcp-agent
+    __start_service neutron-l3-agent
 
 }
 
 function deploy_l2_vlan_infrastructure_compute()
 {
-    openstack-config --set /etc/neutron/plugins/ml2/openvswitch_agent.ini ovs bridge_mappings provider:br-provider
+    __ml2_config_file='/etc/neutron/plugins/ml2/ml2_conf.ini'
+    __ovs_config_file='/etc/neutron/plugins/ml2/openvswitch_agent.ini'
 
-    openstack-config --set /etc/neutron/plugins/ml2/openvswitch_agent.ini securitygroup firewall_driver iptables_hybrid
+    # OpenVSWitch
+    #  [agent]
+    openstack-config --set ${__ovs_config_file} agent tunnel_types 'vxlan'
+    openstack-config --set ${__ovs_config_file} agent l2_population 'True'
+    #  [ovs]
+    openstack-config --set ${__ovs_config_file} ovs local_ip "$(hostname --ip-address)"
+    openstack-config --set ${__ovs_config_file} ovs bridge_mappings 'provider:br-provider'
+    #  [securitygroup]
+    openstack-config --set ${__ovs_config_file} securitygroup firewall_driver 'iptables_hybrid'
+    openstack-config --set ${__ovs_config_file} securitygroup enable_security_group 'True'
 
     __enable_service openvswitch
     __start_service openvswitch
